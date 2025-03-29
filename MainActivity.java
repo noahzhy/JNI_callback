4b0a456692a346364aaecb3873f98df73f517704
Haoyu Zhang <noahzhy@gmail.com>
2025-03-29 23:35:55 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -42,4 +42,84 @@ public class MainActivity extends AppCompatActivity {
         );
 
     }
+
+    public static byte[] YUV420_888ToNV21(Image image, int rotation) {
+        assert (image.getFormat() == ImageFormat.YUV_420_888);
+
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final int size = width * height;
+        byte[] nv21 = new byte[size + size / 2];
+
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+        int rowStride = planes[0].getRowStride();
+        int pixelStride = planes[1].getPixelStride();
+
+        // Copy Y data
+        for (int i = 0; i < height; i++) {
+            yBuffer.position(i * rowStride);
+            yBuffer.get(nv21, i * width, width);
+        }
+
+        // Copy UV data
+        int uvHeight = height / 2;
+        int offset = size;
+        for (int i = 0; i < uvHeight; i++) {
+            for (int j = 0; j < width / 2; j++) {
+                int index = i * rowStride / 2 + j * pixelStride;
+                nv21[offset++] = vBuffer.get(index);
+                nv21[offset++] = uBuffer.get(index);
+            }
+        }
+
+        return rotateNV21(nv21, width, height, rotation);
+    }
+
+    private static byte[] rotateNV21(byte[] data, int width, int height, int rotation) {
+        if (rotation == 0) return data;
+
+        byte[] rotated = new byte[data.length];
+        int size = width * height;
+        int uvSize = size / 2;
+
+        if (rotation == 90) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    rotated[x * height + (height - y - 1)] = data[y * width + x];
+                }
+            }
+            for (int y = 0; y < height / 2; y++) {
+                for (int x = 0; x < width / 2; x++) {
+                    int newPos = (x * height + (height - y * 2 - 2)) + size;
+                    rotated[newPos] = data[size + y * width + x * 2];
+                    rotated[newPos + 1] = data[size + y * width + x * 2 + 1];
+                }
+            }
+        } else if (rotation == 180) {
+            for (int i = 0; i < size; i++) {
+                rotated[size - 1 - i] = data[i];
+            }
+            for (int i = 0; i < uvSize; i += 2) {
+                rotated[size + uvSize - 2 - i] = data[size + i];
+                rotated[size + uvSize - 1 - i] = data[size + i + 1];
+            }
+        } else if (rotation == 270) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    rotated[(width - x - 1) * height + y] = data[y * width + x];
+                }
+            }
+            for (int y = 0; y < height / 2; y++) {
+                for (int x = 0; x < width / 2; x++) {
+                    int newPos = ((width - x - 1) * height + y * 2) + size;
+                    rotated[newPos] = data[size + y * width + x * 2];
+                    rotated[newPos + 1] = data[size + y * width + x * 2 + 1];
+                }
+            }
+        }
+        return rotated;
+    }
 }
