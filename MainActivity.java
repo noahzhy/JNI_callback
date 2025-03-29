96795642d75c049b131c72a693001c0df5b7817f
Haoyu Zhang <noahzhy@gmail.com>
2025-03-30 00:22:19 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,25 @@ package com.example.testing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.example.testing.Sdk.OnSubProgressListener;
 import com.example.testing.databinding.ActivityMainBinding;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +29,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Sdk sdk = new Sdk();
-
+    
     private ActivityMainBinding binding;
-
     private ProgressBar progressBar;
-
+    private ImageView iv_img;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +45,75 @@ public class MainActivity extends AppCompatActivity {
 
         sdk.download(
             "xx.png",
-                new OnSubProgressListener() {
-                    @Override
-                    public void onProgressChange(Sdk.Point point) {
-                        Log.d("MainActivity", "dowload progress: " + point.x + "," + point.y);
-                        progressBar.setProgress((int) point.x);
-                    }
+                point -> {
+                    Log.d("MainActivity", "dowload progress: " + point.x + "," + point.y);
+                    progressBar.setProgress((int) point.x);
                 }
         );
 
+        iv_img = findViewById(R.id.iv_img);
+        BitmapDrawable bd = (BitmapDrawable) iv_img.getDrawable();
+        Bitmap b = bd.getBitmap();
+        byte[] nv21 = BitmapToNV21(b);
+
+        int width = b.getWidth();
+        int height = b.getHeight();
+        int rotation = 270;
+        byte[] bytes = rotateNV21(nv21, width, height, rotation);
+
+        FileOutputStream fos = null;
+        try {
+            File file = new File(getExternalFilesDir(null), "test.jpg");
+            String filePath = file.getAbsolutePath();
+            Log.d("test_path", filePath);
+            fos = new FileOutputStream(file);
+            YuvImage yuvImage = new YuvImage(bytes, ImageFormat.NV21, height, width, null);
+            yuvImage.compressToJpeg(new Rect(0, 0, height, width), 100, fos);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                assert fos != null;
+                fos.close();
+            } catch (IOException e) {
+                Log.e("error", e.toString());
+            }
+        }
+    }
+
+    public static byte[] BitmapToNV21(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] argb = new int[width * height];
+        bitmap.getPixels(argb, 0, width, 0, 0, width, height);
+        byte[] yuv = new byte[width * height * 3 / 2];
+        encodeYUV420SP(yuv, argb, width, height);
+//        bitmap.recycle();
+        return yuv;
+    }
+
+    private static void encodeYUV420SP(byte[] yuv420sp, int[] argb, int width, int height) {
+        final int frameSize = width * height;
+        int yIndex = 0;
+        int uvIndex = frameSize;
+        int R, G, B, Y, U, V;
+        int index = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                R = (argb[index] & 0xff0000) >> 16;
+                G = (argb[index] & 0xff00) >> 8;
+                B = (argb[index] & 0xff);
+                Y = ((66 * R + 129 * G + 25 * B + 128) >> 8) + 16;
+                U = ((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128;
+                V = ((112 * R - 94 * G - 18 * B + 128) >> 8) + 128;
+                yuv420sp[yIndex++] = (byte) Math.min(Y, 255);
+                if (j % 2 == 0 && index % 2 == 0) {
+                    yuv420sp[uvIndex++] = (byte) Math.min(V, 255);
+                    yuv420sp[uvIndex++] = (byte) Math.min(U, 255);
+                }
+                index++;
+            }
+        }
     }
 
     public static byte[] YUV420_888ToNV21(Image image, int rotation) {
@@ -112,11 +185,13 @@ public class MainActivity extends AppCompatActivity {
                     rotated[(width - x - 1) * height + y] = data[y * width + x];
                 }
             }
-            for (int y = 0; y < height / 2; y++) {
-                for (int x = 0; x < width / 2; x++) {
-                    int newPos = ((width - x - 1) * height + y * 2) + size;
-                    rotated[newPos] = data[size + y * width + x * 2];
-                    rotated[newPos + 1] = data[size + y * width + x * 2 + 1];
+            int i = size;
+            for (int x = width - 1; x >= 0; x -= 2) {
+                for (int y = 0; y < height / 2; y++) {
+                    int newPos = size + y * width + x;
+                    rotated[i] = data[newPos - 1];
+                    rotated[i + 1] = data[newPos];
+                    i += 2;
                 }
             }
         }
